80ca70a0851fa48b3e2c22783c6395d16e125ee0
gizmore <gizmore@wechall.net>
2026-09-18 04:07:27 +0200

    'more korean'

---
+++ lang/open_times_ko.php
@@ -0,0 +1,14 @@
+<?php
+return [
+	'open_times' => '영업시간',
+	'open_times_placeholder' => '월~금 09:00~18:00; 토 10:00-14:00',
+	'open_times_examples' => '예',
+	'open_times_evening' => '저녁',
+	'open_times_help' => 'OpenStreetMap 구문. 세미콜론으로 규칙을 구분합니다. 휴무일에는 \'off\'를 사용하세요.',
+	'open_times_not_set' => '지정되지 않음',
+	'enum_open' => '열기',
+	'enum_open_now' => '지금 열기',
+	'enum_closed' => '휴무',
+	'enum_closed_now' => '지금은 문을 닫음',
+	'enum_unknown' => '알 수 없음',
+];
